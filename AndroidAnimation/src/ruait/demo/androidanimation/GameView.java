ac95fba09f92d66cd2f4636f08e8373f8b95edd3
thanhbc <thanhbc1990@gmail.com>
2013-08-29 16:23:22 +0700

    Change View from GameView to SurfaceView
    Signed-off-by: thanhbc <thanhbc1990@gmail.com>

--- AndroidAnimation/src/ruait/demo/androidanimation/GameView.java
+++ AndroidAnimation/src/ruait/demo/androidanimation/GameView.java
@@ -5,13 +5,19 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.view.SurfaceHolder;
+import android.view.SurfaceHolder.Callback;
+import android.view.SurfaceView;
 import android.view.View;
 
-public class GameView extends View {
+public class GameView extends SurfaceView implements Callback {
 	private Bitmap bm;
+	private SurfaceHolder holder;
 
 	public GameView(Context context) {
 		super(context);
+		holder = getHolder();
+		holder.addCallback(this);
 		bm = BitmapFactory.decodeResource(getResources(),
 				R.drawable.ic_launcher);
 	}
@@ -21,5 +27,21 @@ public class GameView extends View {
 		canvas.drawColor(Color.BLACK);
 		canvas.drawBitmap(bm, 10, 10, null);
 
+	}
+
+	@Override
+	public void surfaceChanged(SurfaceHolder holder, int format, int width,
+			int height) {
+	}
+
+	@Override
+	public void surfaceCreated(SurfaceHolder holder) {
+		Canvas c = holder.lockCanvas();
+		onDraw(c);
+		holder.unlockCanvasAndPost(c);
+	}
+
+	@Override
+	public void surfaceDestroyed(SurfaceHolder holder) {
 	}
 }
